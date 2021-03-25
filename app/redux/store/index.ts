90939be7bb7e0fb9a49225dc01ed2465a53533e8
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-03-25 18:05:40 +0100

    📘 Add redux persist implementation.

--- app/redux/store/index.ts
+++ app/redux/store/index.ts
@@ -1,14 +1,35 @@
 
 // import redux
-import {createStore, combineReducers} from 'redux';
+import { createStore, combineReducers, AnyAction } from 'redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {persistReducer, persistStore } from 'redux-persist';
 
 // import redux reducers
 import {} from '../reducers';
+import { PersistConfig } from 'redux-persist/es/types';
 
 // define app reducers
 const appReducers = combineReducers({
   // you need to add your reducers here
 });
 
+// define root reducer
+const rootReducer = (state:Object, action:AnyAction) => {
+  return appReducers(state, action);
+}
+
+// config persist store
+const persistConfig: PersistConfig<Object> = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['']
+}
+
+// define persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 // export store
-export const store = createStore(appReducers);
+export const store = createStore(persistedReducer);
+
+// export persistor
+export const persistor = persistStore(store);
