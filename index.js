2abe5ed53374b16c167ada0208f694dd9f6d200d
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-03-27 12:09:01 +0100

    🛴 Add network logger.

--- index.js
+++ index.js
@@ -5,5 +5,8 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {startNetworkLogging} from 'react-native-network-logger';
+
+startNetworkLogging();
 
 AppRegistry.registerComponent(appName, () => App);
