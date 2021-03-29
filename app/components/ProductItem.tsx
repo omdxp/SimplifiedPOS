31768b376dc59cb58003b1926541fd88de29f9ce
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-03-29 13:16:01 +0100

    🕶 Create product item component.

---
+++ app/components/ProductItem.tsx
@@ -0,0 +1,14 @@
+
+// import react native
+import React from 'react';
+import {Text, View} from 'react-native';
+
+// export ProductItem component
+export default function ProductItem() {
+  return (
+    <View>
+      <Text>ProductItem component created!</Text>
+    </View>
+  );
+}
+
