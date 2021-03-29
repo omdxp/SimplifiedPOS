7a61de5b5a52ab6b8b1d62cd82ceb780619422cb
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-03-29 14:15:09 +0100

    🧱 Integration redux in the app.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
 // import react native
 import React from 'react';
 
+// import redux
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/es/integration/react';
+import {store, persistor} from './app/redux/store';
+
 // import navigation container
 import {NavigationContainer} from '@react-navigation/native';
 
@@ -9,8 +14,12 @@ import Navigation from './app/screens/Navigation';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Navigation />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <PersistGate persistor={persistor}>
+          <Navigation />
+        </PersistGate>
+      </NavigationContainer>
+    </Provider>
   );
 }
