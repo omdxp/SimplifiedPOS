a4d83ff32d639d3785b276da7eaef71a3d974ae4
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-03-29 11:52:51 +0100

    🦼 Adding cart component.

---
+++ app/components/Cart.tsx
@@ -0,0 +1,13 @@
+// import react native
+import React, {FC} from 'react';
+import {Text, View} from 'react-native';
+
+// export Cart component
+const Cart: FC = (): JSX.Element => {
+  return (
+    <View>
+      <Text>Cart component created!</Text>
+    </View>
+  );
+};
+export default Cart;
--- app/screens/Home/ui/HomeUI.tsx
+++ app/screens/Home/ui/HomeUI.tsx
@@ -1,16 +1,9 @@
 // import react native
 import React, {FC} from 'react';
-import {Text, View, FlatList} from 'react-native';
+import {View} from 'react-native';
 
 // import components
-import ProductComponent from '../../../components/Product';
 import ProductsList from '../../../components/ProductsList';
-
-// import screens
-import ProductDetailsScreen from '../../ProductDetails/ui/ProductDetailsUI';
-
-// import navigation
-import {useNavigation} from '@react-navigation/native';
 
 // import global styles
 import globalStyles from '../../../styles';
@@ -23,12 +16,9 @@ import {DATA} from '../../../test_data';
 
 // export Home UI
 const HomeUI: FC = (): JSX.Element => {
-  // use navigation
-  const navigation = useNavigation();
   return (
     <View style={globalStyles.mainView}>
       <ProductsList data={DATA} />
-      {/* <ProductDetailsScreen image={DATA[0].image} title={DATA[0].title} description={DATA[0].title} price={DATA[0].price} /> */}
     </View>
   );
 };
