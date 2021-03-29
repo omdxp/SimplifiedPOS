87da8ea3bacd7544794c56fa309ab2bafa624447
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-03-29 13:23:52 +0100

    ⏰ Styling cart component.

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -1,6 +1,11 @@
 // import react native
 import React, {FC} from 'react';
 import {Text, View, FlatList} from 'react-native';
+
+// import components
+import ProductItemComponent from '../components/ProductItem';
+
+// import test data
 import {DATA} from '../test_data';
 
 // export Cart component
@@ -10,7 +15,9 @@ const Cart: FC = (): JSX.Element => {
       <Text style={{fontSize: 30, fontWeight: 'bold'}}>My Cart</Text>
       <FlatList
         data={DATA}
-        renderItem={({item}) => <Text>{item.title}</Text>}
+        renderItem={({item}) => (
+          <ProductItemComponent name={item.title} quantity={2} />
+        )}
         keyExtractor={(_, index) => index.toString()}
       />
     </View>
--- app/components/ProductItem.tsx
+++ app/components/ProductItem.tsx
@@ -1,14 +1,20 @@
-
 // import react native
-import React from 'react';
+import React, {FC} from 'react';
 import {Text, View} from 'react-native';
 
-// export ProductItem component
-export default function ProductItem() {
-  return (
-    <View>
-      <Text>ProductItem component created!</Text>
-    </View>
-  );
+// define product item props interface
+interface ProductItemProps {
+  name: string;
+  quantity: number;
 }
 
+// export ProductItem component
+const ProductItem: FC<ProductItemProps> = ({name, quantity}): JSX.Element => {
+  return (
+    <View>
+      <Text>{name.length > 7 ? name.substring(0, 20) + '...' : name}</Text>
+      <Text style={{alignSelf: 'flex-end'}}>x ({quantity})</Text>
+    </View>
+  );
+};
+export default ProductItem;
