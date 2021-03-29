b2f33a470ce6fc3f38e02176dc7be772535d547c
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-03-29 09:56:21 +0100

    🏍 Define test data.

--- app/screens/Home/ui/HomeUI.tsx
+++ app/screens/Home/ui/HomeUI.tsx
@@ -6,14 +6,16 @@ import { Text, View } from 'react-native';
 // import components
 import ProductComponent from '../../../components/Product';
 
+// import global styles
+import globalStyles from '../../../styles';
+
 // import Home functions
 // import {} from '../functions';
 
 // export Home UI
 export default function HomeUI() {
   return (
-    <View>
-      <Text>Home UI created!</Text>
+    <View style={globalStyles.mainView}>
       <ProductComponent title={'Product name'} price={'Product price'} />
     </View>
   );
---
+++ app/test_data/index.ts
@@ -0,0 +1,52 @@
+// define data object type
+type DataObject = {
+  title: string;
+  image: string;
+  price: string;
+};
+// define data type
+type DataType = DataObject[];
+// export sample data
+export const DATA: DataType = [
+  {
+    title: 'Luke Skywalker',
+    image:
+      'https://images-na.ssl-images-amazon.com/images/I/81Dlml4kFPL._AC_SL1500_.jpg',
+    price: '$25',
+  },
+  {
+    title: 'GTA V',
+    image:
+      'https://frugalgaming.co.uk/wp-content/uploads/2019/05/gta5premiumbase2.jpg',
+    price: '$50',
+  },
+  {
+    title: 'Uncharted 4',
+    image:
+      'https://images-na.ssl-images-amazon.com/images/I/71hcX5qwKNL._AC_SX385_.jpg',
+    price: '$40',
+  },
+  {
+    title: 'Shadow of the Tomb Raider',
+    image:
+      'https://images-na.ssl-images-amazon.com/images/I/81KqDnL5jZL._SL1221_.jpg',
+    price: '$60',
+  },
+  {
+    title: 'Need For Speed Heat',
+    image:
+      'https://media.contentapi.ea.com/content/dam/need-for-speed/nfs-heat/common/nfsh-gamebox-keyart-1x1.jpg.adapt.crop1x1.767w.jpg',
+    price: '$30',
+  },
+  {
+    title: 'DOTA 2',
+    image:
+      'https://s3-eu-central-1.amazonaws.com/www-staging.esports.com/WP%20Media%20Folder%20-%20esports-com//var/app/current/web/app/uploads/2020/06/dota-2-header.jpg',
+    price: '$0',
+  },
+  {
+    title: 'Sekiro: Shadow Die Twice',
+    image: 'https://upload.wikimedia.org/wikipedia/en/6/6e/Sekiro_art.jpg',
+    price: '$20',
+  },
+];
