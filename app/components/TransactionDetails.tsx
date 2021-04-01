fa249967be399bab7178baeadf8a5bc1107ffab2
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-04-01 10:10:42 +0100

    ♨ Adding transaction related components.

---
+++ app/components/TransactionDetails.tsx
@@ -0,0 +1,13 @@
+// import react native
+import React, {FC} from 'react';
+import {Text, View} from 'react-native';
+
+// export TransactionDetails component
+const TransactionDetails: FC = (): JSX.Element => {
+  return (
+    <View>
+      <Text>TransactionDetails component created!</Text>
+    </View>
+  );
+};
+export default TransactionDetails;
---
+++ app/components/TransactionTile.tsx
@@ -0,0 +1,13 @@
+// import react native
+import React, {FC} from 'react';
+import {Text, View} from 'react-native';
+
+// export TransactionTile component
+const TransactionTile: FC = (): JSX.Element => {
+  return (
+    <View>
+      <Text>TransactionTile component created!</Text>
+    </View>
+  );
+};
+export default TransactionTile;
