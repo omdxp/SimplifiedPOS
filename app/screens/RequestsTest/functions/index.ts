0cb27dea6a21f4448e1badba4f3785305e416cae
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-04-04 10:13:16 +0100

    🎡 Testing requests to backend.

--- app/screens/RequestsTest/functions/index.ts
+++ app/screens/RequestsTest/functions/index.ts
@@ -1,2 +1,14 @@
 // write your RequestsTest functions here
-export {};
+export const addProduct = async () => {
+  return await fetch('http://localhost:5000/add-product', {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    // body: JSON.stringify({
+    //   firstParam: 'My first param value.',
+    //   secondParam: 'My second param value.',
+    // }),
+  });
+};
--- app/screens/RequestsTest/ui/RequestsTestUI.tsx
+++ app/screens/RequestsTest/ui/RequestsTestUI.tsx
@@ -1,15 +1,47 @@
 // import react native
-import React, {FC} from 'react';
-import {Text, View} from 'react-native';
+import React, {FC, useState} from 'react';
+import {Text, View, TouchableOpacity} from 'react-native';
 
 // import RequestsTest functions
-import {} from '../functions';
+import {addProduct} from '../functions';
+
+// import styles
+import globalStyles from '../../../styles';
+import {Colors} from '../../../styles/colors';
 
 // export RequestsTest UI
 const RequestsTestUI: FC = (): JSX.Element => {
+  // use result state
+  const [result, setResult] = useState('');
+  // on fetch press
+  const fetchPress = async () => {
+    try {
+      const resp: Response = await addProduct();
+      console.log('resp:', resp);
+      setResult(JSON.stringify(resp));
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
-    <View>
-      <Text>RequestsTest UI created!</Text>
+    <View
+      style={[
+        globalStyles.mainView,
+        {alignItems: 'center', justifyContent: 'center'},
+      ]}>
+      <TouchableOpacity
+        style={{
+          backgroundColor: Colors.mainColor,
+          margin: 10,
+          padding: 10,
+          borderRadius: 30,
+        }}
+        onPress={fetchPress}>
+        <Text style={{fontSize: 50, color: Colors.white}}>Fetch</Text>
+      </TouchableOpacity>
+      <Text style={{alignSelf: 'flex-start', fontSize: 25}}>
+        Result: {result}
+      </Text>
     </View>
   );
 };
